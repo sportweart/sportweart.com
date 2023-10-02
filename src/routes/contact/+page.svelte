14e235f6988c45dc72e090d1434285c5c64e43c2
NelsonDJCR <dev@nelsondjcr.com>
2023-10-02 04:34:32 -0500

    build: contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,164 @@
+<svelte:head>
+	<title>....</title>
+</svelte:head>
+<section class="position-relative xs-bg-cover" style="background-image: url(assets/images/banner/banner-contact.jpg);">
+	<div class="xs-overlay" />
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="xs-banner-area mt-5">
+					<h1 class="xs-banner-title">Contact <span>Us!</span></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="xs-section-padding xs-pb-sm">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-8 mx-auto">
+				<div class="xs-section-heading text-center">
+					<h2>Find <span>Our GYMS</span></h2>
+					<p>World is committed to making participation in the event a harassment free experience for everyone, regardless.</p>
+				</div>
+			</div>
+		</div>
+		<!-- .row END -->
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="xs-contact-list">
+					<h3><i class="icon icon-location" /> Manhattan, NY</h3>
+					<p><a href="https://www.google.com/maps/place/New+York,+NY,+USA/@40.6976637,-74.119764,11z/data=!3m1!4b1!4m5!3m4!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728" target="_blank">GymVast, 18 East 50th Street, 4th Floor,<br /> New York, NY 10022</a></p>
+					<ul>
+						<li><span>PHONE:</span> <a href="tel:+360%202254%202265">+360 2254 2265</a></li>
+						<li>
+							<span>EMAIL:</span>
+							<p><a href="mailto:contact@gymvast.com">contact@gymvast.com</a></p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="xs-contact-list">
+					<h3><i class="icon icon-location" /> Union City, NY</h3>
+					<p><a href="https://www.google.com/maps/place/New+York,+NY,+USA/@40.6976637,-74.119764,11z/data=!3m1!4b1!4m5!3m4!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728" target="_blank">GymVast, 18 East 50th Street, 4th Floor,<br /> New York, NY 10022</a></p>
+					<ul>
+						<li>
+							<span>PHONE:</span>
+							<p><a href="tel:+360%202254%202265">+360 2254 2265</a></p>
+						</li>
+						<li>
+							<span>EMAIL:</span>
+							<p><a href="mailto:contact@gymvast.com">contact@gymvast.com</a></p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="xs-contact-list">
+					<h3><i class="icon icon-location" /> Sought Gate, La</h3>
+					<p><a href="https://www.google.com/maps/place/New+York,+NY,+USA/@40.6976637,-74.119764,11z/data=!3m1!4b1!4m5!3m4!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728" target="_blank">GymVast, 18 East 50th Street, 4th Floor,<br /> New York, NY 10022</a></p>
+					<ul>
+						<li>
+							<span>PHONE:</span>
+							<p><a href="tel:+360%202254%202265">+360 2254 2265</a></p>
+						</li>
+						<li>
+							<span>EMAIL:</span>
+							<p><a href="mailto:contact@gymvast.com">contact@gymvast.com</a></p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- .row END -->
+	</div>
+	<!-- .container END -->
+</section>
+<!-- Contact End -->
+
+<!-- Contact Form -->
+<div id="xs-map" class="xs-map-contact" />
+<!-- Contact Form End -->
+
+<!-- Contact Form -->
+<section class="xs-pb-sm">
+	<div class="container">
+		<div class="xs-contact-form">
+			<div class="row">
+				<div class="col-lg-7 col-md-8 mx-auto">
+					<div class="xs-section-heading text-center">
+						<h2>Send <span>Message</span></h2>
+						<p>World is committed to making participation in the event harass ment free on experience for everyone, regardless.</p>
+					</div>
+				</div>
+			</div>
+			<form action="#" class="xs-form" id="xs-contact-form">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group xs-form-anim">
+							<label class="input-label" for="xs-name">Your name</label>
+							<input type="text" id="xs-name" class="form-control" />
+						</div>
+						<div class="form-group xs-form-anim">
+							<label class="input-label" for="xs-email">Your email address</label>
+							<input type="text" id="xs-email" class="form-control" />
+						</div>
+						<div class="form-group xs-form-anim">
+							<label class="input-label" for="xs-phone">Mobile number</label>
+							<input type="text" id="xs-phone" class="form-control" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group xs-form-anim xs-message-box">
+							<label class="input-label" for="xs-message">Your massage</label>
+							<textarea id="xs-message" class="form-control" />
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<p class="form-text text-muted xs-mt-50">We do not sell, share, trade your information with anyone for any reason. We won’t give spam mails.</p>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group xs-mt-60 text-right">
+							<button type="submit" id="xs_contact_submit" class="btn btn-primary">Submit Now <span class="icon icon-plus" /></button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+		<!-- .xs-contact-form END -->
+	</div>
+	<!-- .container END -->
+</section>
+<!-- Contact Form End -->
+
+<!-- Brand -->
+<div class="xs-cyan-bg xs-section-padding-xs">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-12">
+				<div class="owl-carousel owl-theme xs-brand-owl">
+					<div class="item">
+						<img src="assets/images/brand/brand1.png" alt="brand" />
+					</div>
+					<div class="item">
+						<img src="assets/images/brand/brand2.png" alt="brand" />
+					</div>
+					<div class="item">
+						<img src="assets/images/brand/brand3.png" alt="brand" />
+					</div>
+					<div class="item">
+						<img src="assets/images/brand/brand4.png" alt="brand" />
+					</div>
+					<div class="item">
+						<img src="assets/images/brand/brand5.png" alt="brand" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
