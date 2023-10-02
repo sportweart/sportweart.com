1508019b46f1b3693872496a421d25570cd65859
NelsonDJCR <dev@nelsondjcr.com>
2023-10-02 04:34:06 -0500

    build: home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,111 @@
 	import { preparePageTransition } from '$lib/config/page-transition';
 	import 'aos/dist/aos.css';
 	import AOS from 'aos';
-	import { onMount } from 'svelte';
+	import { afterUpdate, onMount } from 'svelte';
+	import { INFO_COMPANY, SOCIAL } from '$lib/data/info';
 	onMount(() => {
 		AOS.init();
 	});
 	preparePageTransition();
+	let current_url;
+	let text_menu_c ='text-page-main'
+	afterUpdate(() => {
+		current_url = window.location.pathname;
+		if (current_url == '/') {
+			text_menu_c = 'text-black'
+		}else{
+			text_menu_c = 'text-page-main'
+		}
+	});
 </script>
 
-<main class="text-slate-700 max-w-4xl m-auto">
-	<div class="flex-col items-center justify-center py-4 px-4">
-		<h1 class="text-4xl font-bold mt-4">Template SvelteKit Mazter</h1>
-		<slot />
+<header class="elementskit-header xs-header-transparent" style="z-index: 40;">
+	<div class="xs-container">
+		<div class="xs-navbar">
+			<a class="xs-navbar-brand" href="index.html">
+				<img src="assets/images/logo/logo-white.png" class="w-32 rounded-lg mt-2" alt="navbar logo" />
+			</a>
+			<nav class="elementskit-navbar ml-auto">
+				<button class="elementskit-menu-hamburger elementskit-menu-toggler">
+					<span class="elementskit-menu-hamburger-icon" />
+					<span class="elementskit-menu-hamburger-icon" />
+					<span class="elementskit-menu-hamburger-icon" />
+				</button>
+				<div class="elementskit-menu-container elementskit-menu-offcanvas-elements" style="background: black;">
+					<ul class="elementskit-navbar-nav nav-alignment-dynamic">
+						<li>
+							<a href="/" class={text_menu_c} >Inicio</a>
+						</li>
+						<li>
+							<a href="/store" class={text_menu_c}>Tienda</a>
+						</li>
+						<li>
+							<a href="/contact" class={text_menu_c}>Contacto</a>
+						</li>
+					</ul>
+				</div>
+				<div class="elementskit-menu-overlay elementskit-menu-offcanvas-elements elementskit-menu-toggler" />
+			</nav>
+		</div>
 	</div>
-</main>
+</header>
+<slot />
+<footer class="footer-1">
+	<div class="xs-footer-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6">
+					<div class="footer-widget xs-mr-35">
+						<div class="xs-footer-logo">
+							<img src="assets/images/logo/logo-white.png" class="w-32" alt="logo" />
+						</div>
+						<p>Tenemos un montón de piezas divertidas de equipos construidos en cada parte de tu cuerpo y desde boty en constructores para que los ejecuten..</p>
+						<ul class="xs-footer-contact-info">
+							<li>
+								<i class="icon icon-location" />
+								<p>{INFO_COMPANY.ADDRESS}</p>
+							</li>
+							<li>
+								<i class="icon icon-phone" />
+								<p>
+									<a class="xs-phone-number" href="tel:+65-094-778-622"> {INFO_COMPANY.PHONE} </a>
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="footer-widget">
+						<h3 class="widget-title">Páginas</h3>
+						<div class="media">
+							<div class="media-body">
+								<ul class="list-unstyled">
+									<li><a href="/">Inicio</a></li>
+									<li><a href="/store">Tienda</a></li>
+									<li><a href="/contact">Contacto</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="xs-footer-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 align-self-center">
+					<p>©Copyright SPORTWEART Todos los derechos reservados.</p>
+				</div>
+				<div class="col-md-6">
+					<ul class="list-inline text-center text-md-right">
+						<li><a target="_blank" href={SOCIAL.FACEBOOK} class="icon icon-facebook" /></li>
+						<li><a target="_blank" href={SOCIAL.WHATSAPP} class="icon icon-phone" /></li>
+						<li><a target="_blank" href={SOCIAL.INSTAGRAM} class="icon icon-instagram" /></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,87 @@
 <svelte:head>
-	<title>Template Mazter SvelteKit</title>
+	<title>....</title>
 </svelte:head>
 
-<div class="w-[100%] mt-5" data-aos="fade-down">
-	<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-		<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-			<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-				<tr>
-					<th scope="col" colspan="2" class="px-6 py-3"> Dependences</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> PWA </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> View Transitions for use: <span class="bg-red-900 p-1 px-2 mx-3 rounded-lg">style:--tag={'{'}'title'{'}'}</span> beetween elements </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> Tailwind </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> <a href="https://github.com/KamyarLajani/svelte-yup">Yup Validation </a> </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4"> <a href="https://michalsnik.github.io/aos/" target="_blank">AOS</a> </td>
-				</tr>
-				<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-					<td class="px-6 py-4 flex justify-around">
-						ICONS:
-						<a href="https://akveo.github.io/eva-icons" target="_blank">akveo</a>
-						<a href="https://akaricons.com/?ref=uigoodies.com" target="_blank">akaricons</a>
-						<a href="https://tablericons.com" target="_blank">tablericons</a>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+<section class="" data-scrollax-parent="true" id="home">
+	<div class="container xs-clips-wraper">
+		<div class="xs-clips floating_animation">
+			<img src="assets/images/shape/banner-light-clips.png" alt="clip" />
+		</div>
 	</div>
-</div>
+	<div class="h-[90vh] flex justify-center items-center relative">
+		<h2 class="z-30  -ml-[21rem] absolute text-[#FCFE03] font-[800] text-[12rem] floating_animation" style="z-index: 21;">FITN</h2>
+		<img src="/assets/images/slider-light/slide-3.png" class="w-[600px]" style="z-index: 22;" alt="shape" />
+		<h2 class="z-30 ml-[25rem] absolute  text-[#FCFE03] font-[800] text-[12rem] floating_animation" style="z-index: 23; ">ESS</h2>
+	</div>
+</section>
 
-<div class="d-flex justify-center items-center">
-	<a href="/home">
-		<img src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg" style:--tag={'taylor'} alt="taylor" class="w-[80px]" />
-	</a>
-	<br />
-	<h1 style:--tag={'title'} class="font-bold">Cristiano</h1>
-</div>
+<section class="xs-section-padding" id="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 mx-auto">
+				<div class="xs-section-heading text-center">
+					<h2 class="text-4xl font-extrabold border-text">¿Por qué unirte a <span class="border-text">Nosotros</span> ?</h2>
+					<p>World is committed to making participation in the event a harassment free experience for everyone, regardless of level of experience.</p>
+				</div>
+			</div>
+		</div><!-- .row END -->
+
+		<div class="row">
+			<div class="col-lg-3 col-md-6">
+				<div class="xs-about-us">
+					<object id="servicesvg" type="image/svg+xml" data="assets/images/services/01.svg"></object>
+					<h3>TONS OF EQUIPMENT</h3>
+					<p>
+						We have heap of fun piece of equi to build down every inh of your for body and from boom.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="xs-about-us" >
+				<object id="servicesvg2" type="image/svg+xml" data="assets/images/services/02.svg"></object>
+					<h3>ROWS OF CARDIO</h3>
+					<p>
+						We have heap of fun piece of equi to build down every inh of your for body and from boom.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+			<div class="xs-about-us" >
+				<object id="servicesvg3" type="image/svg+xml" data="assets/images/services/03.svg"></object>
+					<h3>HEART PUMPING</h3>
+					<p>
+						We have heap of fun piece of equi to build down every inh of your for body and from boom.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+			<div class="xs-about-us" >
+				<object id="servicesvg4" type="image/svg+xml" data="assets/images/services/04.svg"></object>
+					<h3>TONS OF EQUIPMENT</h3>
+					<p>
+						We have heap of fun piece of equi to build down every inh of your for body and from boom.
+					</p>
+				</div>
+			</div>
+		</div><!-- .row END -->
+	</div><!-- .container END -->
+</section>
+
+<section class="xs-section-padding-lg xs-bg-cover parallaxie" style="background-image: url(assets/images/help-light-img.jpg)">
+	<div class="container">
+		<div class="xs-help xs-help-light">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="xs-section-heading">
+						<h2 class="text-4xl font-extrabold border-text">HELPING YOU to GO <br />
+							<span>Since 1971</span>
+						</h2>
+					</div>
+					<p>World is committed to making participation in the event  harass ment free on experience for everyone, regardless of leve of expenc gender by identity and expression oriention disability for personal.</p>
+					<a href="/" class="btn btn-primary text-black">Ver productos <span class="icon icon-plus"></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
