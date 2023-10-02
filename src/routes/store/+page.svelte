6e74c72414e6798db07d176c1f2a39779c9789b9
NelsonDJCR <dev@nelsondjcr.com>
2023-10-02 04:34:43 -0500

    build: store  page

---
+++ src/routes/store/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>....</title>
+</svelte:head>
+<section class="position-relative xs-bg-cover" style="background-image: url(assets/images/banner/banner-contact.jpg);">
+	<div class="xs-overlay" />
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="xs-banner-area mt-5">
+					<h1 class="xs-banner-title">Productos <span>!</span></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
