1501bae6869ccaa48f0ebda1635f2eff44a3a629
NelsonDJCR <dev@nelsondjcr.com>
2023-10-03 16:56:09 -0500

    build: create button whatsapp

---
+++ src/lib/components/ButtonWhatsApp.svelte
@@ -0,0 +1,39 @@
+<!-- WhatsAppButton.svelte -->
+<script>
+	import { INFO_COMPANY, SOCIAL } from "$lib/data/info";
+	const whatsappUrl = SOCIAL.WHATSAPP;
+	function abrirWhatsApp() {
+		window.open(whatsappUrl, '_blank');
+	}
+</script>
+
+<button class="whatsapp-button" on:click={abrirWhatsApp}>
+	<img class="whatsapp-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/598px-WhatsApp.svg.png" alt="WhatsApp" width="30" height="30" />
+	Escríbenos
+</button>
+
+<style>
+	.whatsapp-button {
+		z-index: 50;
+		position: fixed;
+		bottom: 20px;
+		left: 20px;
+		background-color: #25d366;
+		color: #fff;
+		padding: 10px 20px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		font-size: 16px;
+		display: flex; 
+		align-items: center;
+		transition: all 0.5s;
+	}
+	.whatsapp-button:hover {
+		transform: translateY(-5px);
+	}
+
+	.whatsapp-icon {
+		margin-right: 10px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,11 @@
 	import 'aos/dist/aos.css';
 	import { onMount } from 'svelte';
 	import AOS from "aos";
+	import ButtonWhatsApp from '$lib/components/ButtonWhatsApp.svelte';
     onMount(() => {
         AOS.init();
     });
 	preparePageTransition();
 </script>
+<ButtonWhatsApp/>
 <slot />
